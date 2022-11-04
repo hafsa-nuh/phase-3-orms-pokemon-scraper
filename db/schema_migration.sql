5c8267ddcee1ef4138ecde1458db91149cd41013
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-11-04 10:01:40 +0300

    done

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,3 +1,7 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+-- creates pokemon table
+ -- has an autoincrementing id column
+ -- has a name column
+--  has a type column
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
